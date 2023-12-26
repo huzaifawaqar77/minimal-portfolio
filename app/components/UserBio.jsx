47037cafb51773010c6f9dda033b47bfcda97346
huzaifa <huzaifawaqar77@gmail.com>
2023-12-26 11:55:21 +0500

    Update UserBio component styling and content

--- app/components/UserBio.jsx
+++ app/components/UserBio.jsx
@@ -14,14 +14,14 @@ import { FiArchive } from "react-icons/fi";
 
 export default function UserBio() {
   return (
-    <section className="h-full w-1/3">
+    <section className="h-full w-1/3 user-bio-section">
       <div className="flex justify-between">
         <span className="rounded-full relative">
           <Image
             src={profilePic}
             alt="profile picture"
-            width={100}
-            height={100}
+            width={150}
+            height={150}
             className="rounded-full overflow-hidden"
           />
           <span className="absolute bottom-0 right-0 bg-green-700 w-3 h-3 rounded-full"></span>
@@ -56,17 +56,17 @@ export default function UserBio() {
           placeholder="yourname@email.com"
           className=" focus:outline-none p-2 rounded-md bg-gray-800 text-slate-300"
         />
-        <button className="px-2 py-2 bg-gray-400 text-slate-950 font-semibold rounded-md">
+        <button className="px-4 py-1 bg-gray-400 text-slate-950 font-semibold rounded-md">
           Subscribe
         </button>
       </form>
       {/* Bio Section */}
-      <h3 className="text-xl font-bold my-6">Bio</h3>
+      <h3 className="text-xl font-bold my-4">Bio</h3>
       <p className=" text-slate-300 ">
         I am a MERN Stack developer with expertise in ReactJs, NextJs, MongoDB,
         Supabase, Firebase, Tailwind CSS, NodeJS, ExpressJS
       </p>
-      {/* experience */}
+      {/* Experience Section */}
       <div className="flex items-center justify-between my-4 border-b border-slate-800 pb-3">
         <div className="flex items-center gap-2">
           <FiFeather /> <small>3+ Years of Experience</small>
@@ -74,6 +74,18 @@ export default function UserBio() {
         <div className="flex items-center gap-2">
           <FiArchive /> <small>10+ Projects</small>
         </div>
+      </div>
+      {/* Projects Section */}
+      <div>
+        <h3 className="text-xl font-bold my-3">Projects</h3>
+        <ul className="text-slate-300">
+          <li>NextJs Portfolio</li>
+          <li>NextJs Full Stack Ticket Management</li>
+          <li>ReactJs Full Stack Project Management</li>
+          <li>ReactJs Tailswind Nike showcase</li>
+          <li>ReactJs Expense Tracker</li>
+          <li>MERN Stack Booking App</li>
+        </ul>
       </div>
     </section>
   );
